0abf35585674f901cbc273fd1146906b148845af
megha <megha9673@gmail.com>
2018-08-09 01:19:39 +0530

    changed

--- index.js
+++ index.js
@@ -11,30 +11,32 @@ firebase.initializeApp({
 const db = firebase.database();
 //.ref("server/saving-data/fireblog/users");
 console.log('df');
-db.ref('users').on("child_changed", function(snapshot) {
+db.ref('/users/userdata').on("child_added", function(snapshot) {
 	console.log('fqewf');
     var changedUser = snapshot.val();
     console.log(changedUser);
 	console.log("result.body");
-	//const obj = JSON.parse(changedUser.body);
-	console.log('keke');
-	const newUser = {
+	// count++;
+	// var newKey = "user"+count;
+	const newUser = 
+	{
 		email : changedUser.email,
 		name : changedUser.name
 	};
-	return db.ref('userInformation').set(newUser).then(() => {
+	
+	db.ref('userInformation').push(newUser).then(() => {
 		console.log('success');	
 	})
 	var transporter = nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-		    user: 'youremail@gmail.com',
-		    pass: 'yourpassword'
+		    user: 'your Email',
+		    pass: 'password'
 		}
 	});
 
 	var mailOptions = {
-	  from: 'youremail@gmail.com',
+	  from: 'your email',
 	  to: changedUser.email,
 	  subject: 'SetMyTest',
 	  text: 'Dear '+ changedUser.name +' Welcome to our app!!'
